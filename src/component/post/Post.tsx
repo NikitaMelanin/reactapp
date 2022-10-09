427f5dd5a9b70264f1c78dfcccb32e7059ec5cb1
leenwood <george200135@gmail.com>
2022-10-09 12:53:02 +0500

    test commit

---
+++ src/component/post/Post.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Post.css';
+
+function Post() {
+    return (
+        <div>
+            <div className={'border'}>
+                <h1>Post title</h1>
+                <hr/>
+                <p>Post description</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Post;
